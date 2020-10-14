6bdea27252e1a6c77dad49d4d6ab85f5dc83c801
angelo <angelomadara.dev@gmail.com>
2020-10-14 23:09:24 +0800

    firebase

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,66 @@
 <template>
   <div id="app" class="home">
-    <img src="@/assets/logo.png" alt="Vue Logo" id="vue-logo">
     <div class="title">
       What do I need to do today?
     </div>
     <input v-model="myTodo" type="text">
-    <button @click="addToDo">Add</button>
+    <button @click="addToDo()">Add</button>
+    <div v-if="errors !== ''" id="errors">{{ errors }}</div>
+
+    <ul v-if="this.$store.getters.getItems && this.$store.getters.getItems.length > 0">
+      <li v-for="item in this.$store.getters.getItems" :key="item.id">
+        {{ item.title }}
+        <button @click="removeTodo(item.id)" data-id=''>x </button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+import { db } from "@/main";
 
 export default {
+  beforeCreate(){
+    this.items = this.$store.dispatch('setItems')
+  },
   name: 'App',
-  data: function() {
+  data(){
     return {
-      myTodo: ''
+      myTodo: '',
+      errors: '',
+      items : null,
     }
   },
-  components: {
-    // HelloWorld
+  created(){
+    console.log(this.items)
   },
-  mothods:{
-    addToDo : function(){
-      console.log(this.myTodo)
+  methods:{
+    addToDo(){
+      this.errors = ''
+      if(this.myTodo !== ''){
+        db.collection('items').add({
+          title: this.myTodo,
+          created_at: Date.now()
+        }).then(response => {
+          if(response){
+            this.myTodo = ''
+          }
+        }).catch(error =>{
+          this.error = error
+        })
+      }else{
+        this.errors = "Write what you want to do"
+      }
+    },
+    removeTodo(id){
+      if(id){
+        db.collection('items').doc(id).delete().then(response =>{
+          if(response) alert('item is deleted')
+        }).catch(error => {
+          this.errors = error
+        })
+      }
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+const config = {
+  apiKey: "AIzaSyCj3K4nwf4qA8T4Dn_cyK2aCpZzWmiAr-k",
+  authDomain: "personal-270815.firebaseapp.com",
+  databaseURL: "https://personal-270815.firebaseio.com",
+  projectId: "personal-270815",
+  storageBucket: "personal-270815.appspot.com",
+  messagingSenderId: "882705809342",
+  appId: "1:882705809342:web:a4b03595b4991efee383ef"
+}
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
+
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
