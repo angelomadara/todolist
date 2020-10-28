4804c28195943cbcfc35df9b0ce93927ec79d02d
angelo <angelomadara.dev@gmail.com>
2020-10-28 20:03:04 +0800

    hide gcp key

--- src/main.js
+++ src/main.js
@@ -2,17 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import firebase from 'firebase'
+// import dotenv from 'dotenv'
 
 Vue.config.productionTip = false
 
+// require('dotenv').config({ path: './../.env' })
+console.log(process.env)
 const config = {
-  apiKey: "AIzaSyCj3K4nwf4qA8T4Dn_cyK2aCpZzWmiAr-k",
-  authDomain: "personal-270815.firebaseapp.com",
-  databaseURL: "https://personal-270815.firebaseio.com",
-  projectId: "personal-270815",
-  storageBucket: "personal-270815.appspot.com",
-  messagingSenderId: "882705809342",
-  appId: "1:882705809342:web:a4b03595b4991efee383ef"
+  apiKey: process.env.VUE_APP_API_KEY,
+  authDomain: process.env.VUE_APP_AUTHDOMAIN,
+  databaseURL: process.env.VUE_APP_DATABASEURL,
+  projectId: process.env.VUE_APP_PROJECTID,
+  storageBucket: process.env.VUE_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.VUE_APP_MESSAGINGSENDERID,
+  appId: process.env.VUE_APP_APPID
 }
 
 firebase.initializeApp(config)
